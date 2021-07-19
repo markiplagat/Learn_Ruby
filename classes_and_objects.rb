ed797442b12f83f2aa5a12370a995cb1cf779b6a
markiplagat <markkiplagatkipruttoh@gmail.com>
2021-07-19 17:09:01 +0300

    Classes and objets

---
+++ classes_and_objects.rb
@@ -0,0 +1,31 @@
+class Humans
+  def evolve
+    puts "Human Beings evolve"
+  end
+end
+
+class Person < Humans
+  @@persons = 0
+  def initialize(id,name,age)
+    @id = id
+    @name = name
+    @age = age
+    @@persons +=1
+  end
+
+  def intro
+    puts "My name is #{@name} and i am #{@age} years old"
+    puts "and this is my speech: "
+    evolve
+  end
+
+  def self.count_all_people
+    return @@persons
+  end
+end
+
+p = Person.new(1, "mark", 23)
+p1 = Person.new(2,"kip", 34)
+p.intro
+puts Person.count_all_people
+
