895f995d828ad8ebf06c8200a0411514773a9f47
markiplagat <markkiplagatkipruttoh@gmail.com>
2022-02-28 09:39:29 +0300

    Tab method

--- methods.rb
+++ methods.rb
@@ -11,3 +11,19 @@ p "After mutate method: #{a}"
 
 # Stack -> helps ruby keep track of what method is executing as well
 # as where execution should resume when it returns
+
+# Tap method
+class User
+  attr_accessor :name, :age
+
+  def self.my_tap(name, age)
+    User.new.tap do |u|
+      u.name = name
+      u.age = age
+    end
+  end
+end
+
+new_user = User.my_tap('mark', 25)
+
+puts "#{new_user.name.capitalize} is #{new_user.age} years old"
