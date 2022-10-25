28247b78aeb2ce812e93fc8bf71391e97c42e168
markiplagat <markkiplagatkipruttoh@gmail.com>
2022-10-25 21:54:54 +0300

    Fixes

--- moodules.rb
+++ moodules.rb
@@ -3,9 +3,7 @@ def grading_students(grades)
 
   grades.each do |grade|
     if grade > 37 && grade % 5 <= 3
-      while grade % 5 != 0 do
-        grade += grade
-      end
+      grade += grade while grade % 5 != 0
       puts grade
       student_grades << grade
     elsif grade > 30 && grade < 38
@@ -14,7 +12,6 @@ def grading_students(grades)
   end
   puts student_grades
   # Write your code here
-
 end
 grades = [4, 28, 73, 68]
 grading_students(grades)
--- random_numbers.rb
+++ random_numbers.rb
@@ -1,2 +1,2 @@
 # These are random numbers from 100 to 1
-puts rand(1 .. 100)
+puts rand(1..100)
